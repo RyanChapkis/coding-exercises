5c3591f3c7e860ce4068d3ef42571b15830a8515
Ryan Chapkis <rchapkis@ucsc.edu>
2021-12-20 00:31:20 -0800

    Update magic_square.rb
    First I found the sum of the prime diagonal. Then I calculated the sums of each respective row and column. If each of those sums is equal to the sum of the prime diagonal, the 2D array is a magic matrix.

--- code/magic_square.rb
+++ code/magic_square.rb
@@ -3,5 +3,36 @@ class MagicSquare
   # checks if it's a magic square and returns either true or false depending on the result.
   # sequence (https://en.wikipedia.org/wiki/Magic_square)
   def self.validate(square)
+    side_size = square.length
+    prime_diagonal_sum = 0
+
+    for i in 0...side_size
+      prime_diagonal_sum = prime_diagonal_sum + square[i][i]
+    end
+
+    #verify all row sums are equal to prime diagonal sum
+    for i in 0...side_size
+      row_sum = 0
+      for j in 0...side_size
+        row_sum += square[i][j]
+      end
+      if row_sum != prime_diagonal_sum
+        return false
+      end 
+    end
+
+    #verify all column sums are equal to prime diagonal sum
+    for i in 0...side_size
+      col_sum = 0
+      for j in 0...side_size
+        col_sum += square[j][i]
+      end  
+      if prime_diagonal_sum != col_sum
+        return false
+      end  
+    end  
+
+    return true
+
   end
 end
